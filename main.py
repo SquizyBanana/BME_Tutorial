41eb3cbfbfcbd2e68e365afd06ba102d6fbe4e97
sroze <srozendom@gmail.com>
2023-05-01 14:52:58 +0200

    graph works

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import pandas as pd
 import numpy
 name = "main"
 
-def main()
-    var_names = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z'] # Initiate variable names
-    df = pd.read_csv(r'data_q1.csv', names=var_names) # Load the data
-    time = numpy.linspace(0,1024/60, num = 1025)
-    print(time)
-    #df = pd.DataFrame.insert(6, 'time', time)
-    print(df)
-    df = df.cumsum()
-    df.to_numpy()
+class Main:
+    def main(self):
+        var_names = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z'] # Initiate variable names
+        df = pd.read_csv(r'data_q1.csv', names=var_names) # Load the data
+        time = numpy.linspace(0,1024/60, num = 1025)
+        print(time)
+        #df = pd.DataFrame.insert(6, 'time', time)
+        print(df)
+        df = df.cumsum()
+        df.to_numpy()
 
-    #df.plot(x= time ,y=df.acc_x, kind='line')
-    fig, ax = plt.subplots()
-    ax.plot(time, df)
-    fig.show()
+        #df.plot(x= time ,y=df.acc_x, kind='line')
+        fig, ax = plt.subplots()
+        ax.plot(time, df)
+        plt.show()
 
-while name == "main"
-    main()
+if name == "main":
+    main = Main()
+    main.main()
+    #while True:
+    #    main.main()
