a28027aea6d6c6b1afaf419bebb507f9614317a7
sroze <srozendom@gmail.com>
2023-05-01 13:56:01 +0200

    add data

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    import pandas as pd
+    var_names = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z']
+    df = pd.read_csv(r'data\data_q1.csv', names=var_names)  # Load the data
 
 
 # Press the green button in the gutter to run the script.
